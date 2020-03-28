c275f9ab32361849be5faf9517a566b6413f2183
weissk <weiss_kevin@cat.com>
2020-03-28 18:53:30 -0500

    More fixes

--- website/settings.py
+++ website/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
+                           '8+f956ac5=@!49wx%ats_yawjsisjt%ymp_6lggse#gs27+36&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
