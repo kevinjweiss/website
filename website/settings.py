f6844bdd9c835cc71fde11a42026aa3b387c20ff
weissk <weiss_kevin@cat.com>
2020-03-28 14:58:21 -0500

    Allowed host update

--- website/settings.py
+++ website/settings.py
@@ -35,7 +35,7 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = True
     SECURE_REFERRER_POLICY = 'same-origin'
 
-ALLOWED_HOSTS = ['floating-dusk-94418.herokuapp.com']
+ALLOWED_HOSTS = ['floating-dusk-94418.herokuapp.com', 'www.kevinjweiss.com']
 
 
 # Application definition
