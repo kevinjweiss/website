23805b880d198143693076e313cee55c5d033bf4
kevinjweiss <kevin.joseph.weiss@gmail.com>
2020-03-28 20:44:59 -0500

    Still trying to fix database

--- website/settings.py
+++ website/settings.py
@@ -143,5 +143,4 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Heroku: Update database configuration from $DATABASE_URL.
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-del DATABASES['default']['OPTIONS']['sslmode']
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
